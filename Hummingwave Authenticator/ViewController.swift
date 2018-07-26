2b6c58eab99d1cfc6c7f52663795e898ad4ccadd
Rajan Soni <rajan@administrators-macbook-pro-3.local>
2018-07-26 17:36:18 +0530

    -

---
+++ Hummingwave Authenticator/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Hummingwave Authenticator
+//
+//  Created by Rajan Kumar Soni on 26/07/18.
+//  Copyright © 2018 Rajan Kumar Soni. All rights reserved.
+//
+
+import UIKit
+import SwiftOTP
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+   
+}
+
+
